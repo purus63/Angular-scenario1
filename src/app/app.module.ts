25963d1e32474fe1807088597cd4c17715ec6ad9
purus63 <2014dib021@gmail.com>
2019-10-28 14:57:41 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { Component1Component } from './component1/component1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
